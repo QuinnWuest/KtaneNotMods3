4b35e42bae32b3295dea5ec0ea86760a6ad98a55
JakkOfKlubs <qwuest123@gmail.com>
2022-02-15 23:50:13 -0500

    !PP start, !DO cleanup

--- Assets/Modules/!DOh/NotDoubleOhScript.cs
+++ Assets/Modules/!DOh/NotDoubleOhScript.cs
@@ -66,7 +66,8 @@ public class NotDoubleOhScript : MonoBehaviour
         for (int i = 0; i < _grid.Length; i++)
             _grid[i] = "ABCDEFGH".Substring(i / 8, 1) + "ABCDEFGH".Substring(i % 8, 1);
         rnd.ShuffleFisherYates(_grid);
-        Debug.LogFormat("[Not Double-Oh #{0}] Using ruleseed {1}. Grid: {2}", _moduleId, rnd.Seed, _grid.Join(" "));
+        if (rnd.Seed != 1)
+            Debug.LogFormat("[Not Double-Oh #{0}] Using ruleseed {1}.", _moduleId, rnd.Seed);
 
         for (int i = 0; i < LeftSegObjs.Length; i++)
         {
---
+++ Assets/Modules/!PP/NotPerspectivePegsScript.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+public class NotPerspectivePegsScript : MonoBehaviour
+{
+    public KMBombModule Module;
+    public KMBombInfo BombInfo;
+    public KMAudio Audio;
+    public KMColorblindMode ColorblindMode;
+
+    public KMSelectable[] PegSels;
+    public GameObject[] PegObjs;
+    public GameObject[] PegFaceObjs;
+    public Material[] OffMats;
+    public Material[] OnMats;
+    public TextMesh[] ColorblindText;
+
+    private int _moduleId;
+    private static int _moduleIdCounter = 1;
+    private bool _moduleSolved;
+
+    private bool _pegsMoved;
+    private int[] _pegColors = new int[25];
+
+    private bool _colorblindMode;
+
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+        _colorblindMode = ColorblindMode.ColorblindModeActive;
+        SetColorblindMode(_colorblindMode);
+
+        for (int i = 0; i < PegSels.Length; i++)
+            PegSels[i].OnInteract += PegPress(i);
+
+        var tempPegList = new List<int>();
+        for (int i = 0; i < 5; i++)
+        {
+            var tempArr = Enumerable.Range(0, 5).ToArray().Shuffle();
+            for (int j = 0; j < 5; j++)
+            {
+                tempPegList.Add(tempArr[j]);
+                _pegColors[i * 5 + j] = tempPegList[i * 5 + j];
+                PegFaceObjs[i * 5 + j].GetComponent<MeshRenderer>().material = OffMats[_pegColors[i * 5 + j]];
+                ColorblindText[i * 5 + j].text = "BGPRY"[_pegColors[i * 5 + j]].ToString();
+            }
+        }
+    }
+
+    private KMSelectable.OnInteractHandler PegPress(int i)
+    {
+        return delegate ()
+        {
+            if (_moduleSolved)
+                return false;
+            return false;
+        };
+    }
+
+    private void SetColorblindMode(bool mode)
+    {
+        for (int i = 0; i < 25; i++)
+            ColorblindText[i].gameObject.SetActive(mode);
+    }
+
+#pragma warning disable 0414
+    private string TwitchHelpMessage = "Phase 0: !{0} press submit [Presses the submit button.] | 'press' is optional.";
+#pragma warning restore 0414
+
+    private IEnumerator ProcessTwitchCommand(string command)
+    {
+        yield break;
+    }
+
+    private IEnumerator TwitchHandleForcedSolve()
+    {
+        yield break;
+    }
+}
